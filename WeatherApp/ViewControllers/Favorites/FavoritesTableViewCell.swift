f7172122d76424d121c990db6e65f3a6ba682b42
Darina <daryna.massive@gmail.com>
2021-09-26 15:40:07 +0200

    Added:
    Favorites VC table view
    Favorits VC presenter
    Favorites table view cell

---
+++ WeatherApp/ViewControllers/Favorites/FavoritesTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavoritesTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Daryna Polevyk on 26.09.2021.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+
+
+
+}
--- WeatherApp/ViewControllers/Favorites/FavoritesViewController.swift
+++ WeatherApp/ViewControllers/Favorites/FavoritesViewController.swift
@@ -7,14 +7,30 @@
 
 import UIKit
 
-class FavoritesViewController: UIViewController {
-
+class FavoritesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var presenter = PresenterFavoritesVC()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        presenter.view = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Settings.shared.favoriteCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellFav", for: indexPath) // as! FavoritesTableViewCell
+        cell.textLabel?.text = Settings.shared.favoriteCities[indexPath.row]
+        return cell
+    }
     
 
 }
